63ec3b1e748dcd798f132a53f0649e636b5581c5
nelsonelaye <nelsonelaye@gmail.com>
2022-07-09 06:29:39 +0100

    USP - landing page

--- src/Components/Platform/Hero/Hero.js
+++ src/Components/Platform/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => {
       <Wrapper>
         <Uplayer>
           <Left>
-            <Layer1>Best ERP platform for Hospital Management </Layer1>
+            <Layer1>Best ERP platform for Hospital </Layer1>
             <Layer2>
               <Trust>
                 We help hospitals automate and optimize their everyday
